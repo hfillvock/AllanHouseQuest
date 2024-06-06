2ff4c973047345f7e9637d70cd06d7954ee588f9
Henry F <hfillvock@gmail.com>
2024-06-06 20:21:02 -0300

    first commit

---
+++ src/main/java/br/edu/up/allanhousequest/Main.java
@@ -0,0 +1,12 @@
+package br.edu.up.allanhousequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    protected static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
