e7ef4a880dad8ff3ac1bb1574b4c3b2aed5be3b9
JuliusCzare <167374748+JuliusCzare@users.noreply.github.com>
2024-06-08 20:51:18 -0300

    Add files via upload

---
+++ src/main/java/br/edu/up/allanhousequest/daos/CharacterDAO.java
@@ -0,0 +1,17 @@
+package br.edu.up.allanhousequest.daos;
+
+import java.util.List;
+
+public class CharacterDAO {
+
+    public void salvarPersonagem(Character character) {
+        // Implementação para salvar personagem em arquivo
+    }
+
+    public List<Character> listCharacters() {
+        // Implementação para listar personagens
+        return null;
+    }
+
+    // Outros métodos de CRUD
+}
