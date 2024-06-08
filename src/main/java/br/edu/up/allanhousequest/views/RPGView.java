6b04261e1fd13098b6c71651e3cca731e1e12926
JuliusCzare <167374748+JuliusCzare@users.noreply.github.com>
2024-06-08 20:46:56 -0300

    Add files via upload

---
+++ src/main/java/br/edu/up/allanhousequest/views/RPGView.java
@@ -0,0 +1,13 @@
+package br.edu.up.allanhousequest.views;
+
+public class RPGView {
+    
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayCharacterDetails(Character character) {
+        // Implementação para exibir detalhes do personagem
+    }
+    
+}
