243cd9ba662c31f9e59a98fb586e29952f58c221
Allan-House <allanronaldo313@gmail.com>
2024-06-15 23:18:02 -0300

    Update Item.java

--- src/main/java/br/edu/up/allanhousequest/models/Item.java
+++ src/main/java/br/edu/up/allanhousequest/models/Item.java
@@ -4,29 +4,18 @@ import java.io.Serializable;
 
 public class Item implements Serializable{
 	
-	private int id;
 	private String name;
 	private String type;
 	private int value;
 	
 	//Constructor
-	public Item(int id, String name, String type, int value) {
-		this.id = id;
+	public Item(String name, String type, int value) {
 		this.name = name;
 		this.type = type;
 		this.value = value;
 	}
 	
 	//Getters and Setters
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
